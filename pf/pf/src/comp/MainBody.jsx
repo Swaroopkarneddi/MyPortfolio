eda630e638a14a992e31174d2e08408100635c38
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-08-04 20:12:56 +0530

    home

--- pf/pf/src/comp/MainBody.jsx
+++ pf/pf/src/comp/MainBody.jsx
@@ -6,21 +6,29 @@ function MainBody() {
   return (
     <div className="main_body">
       <div className="main_body1">
-        <div>
-          Hi There!<span className="wave">👋 </span> <br />
-          I'M KARNEDDI LAKSHMI NARAYANA SWAROOP
-          <br />
-          <Typer
-            dataText={[
-              "MERN Stack Developer.",
-              "Software Developer.",
-              "Freelancer",
-            ]}
-          />
+        <div className="main_body1_left">
+          <div>
+            Hi There!<span className="wave">👋 </span> <br />
+            I'M{" "}
+            <span className="my_name">KARNEDDI LAKSHMI NARAYANA SWAROOP</span>
+            <br />
+            <Typer
+              dataText={[
+                "MERN Stack Developer.",
+                "Software Developer.",
+                "Freelancer.",
+              ]}
+            />
+          </div>
         </div>
-        <div>img</div>
+        <div className="main_body1_right">
+          <img src="/home_img1.png" alt="" />
+        </div>
       </div>
-      <div className="main_body2">hdbvjdvj</div>
+      <div className="main_body2">
+        <div className="main_body2_top">ksdv</div>
+        <div className="main_body2_bottom">f,v,fv</div>
+      </div>
     </div>
   );
 }
