54ed9bc47d3c012317719d29f7b92f2756170fd5
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-08-04 23:13:50 +0530

    home

--- pf/pf/src/comp/MainBody.jsx
+++ pf/pf/src/comp/MainBody.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Typer from "./subcomp/Typer";
+import Button from "@mui/material/Button";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import "./MainBody.css";
 
 function MainBody() {
@@ -10,7 +15,7 @@ function MainBody() {
           <div>
             Hi There!<span className="wave">👋 </span> <br />
             I'M{" "}
-            <span className="my_name">KARNEDDI LAKSHMI NARAYANA SWAROOP</span>
+            <span className="my_name"> LAKSHMI NARAYANA SWAROOP KARNEDDI</span>
             <br />
             <Typer
               dataText={[
@@ -26,8 +31,79 @@ function MainBody() {
         </div>
       </div>
       <div className="main_body2">
-        <div className="main_body2_top">ksdv</div>
-        <div className="main_body2_bottom">f,v,fv</div>
+        <div className="main_body2_top">
+          <div className="main_body2_top_left">
+            <img src="/test.jpg" alt="" />
+          </div>
+          <div className="main_body2_top_right">
+            <h1>
+              LET ME <span>INTRODUCE</span> MYSELF
+            </h1>
+            <div>
+              <p>
+                I fell in love with programming and I have at least learnt
+                something, I think… 🤷‍♂️
+              </p>
+            </div>
+            <div>
+              <p>
+                I am fluent in classics like{" "}
+                <span>C++, Javascript and React.</span>
+              </p>
+            </div>
+            <div>
+              <p>
+                My field of Interest's are building new{" "}
+                <span>Web Technologies and Products</span> and also in{" "}
+                <span>Coding</span>.
+              </p>
+            </div>
+            <div>
+              <p>
+                Whenever possible, I also apply my passion for developing
+                products with <span>Node.js</span> and{" "}
+                <span>Modern Javascript Library and Frameworks</span> like
+                <span> React.js</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="main_body2_bottom">
+          <div className="main_body2_bottom_txt">FIND ME ON</div>
+          <div className="main_body2_bottom_asd">
+            Feel free to connect with me
+          </div>
+          <div className="soc_icn">
+            <Button
+              className="soc_icn_icn"
+              target="blank"
+              href="https://www.instagram.com/"
+            >
+              <InstagramIcon className="soc_icn_icn_icn" />
+            </Button>
+            <Button
+              className="soc_icn_icn"
+              target="blank"
+              href="https://www.instagram.com/"
+            >
+              <LinkedInIcon className="soc_icn_icn_icn" />
+            </Button>
+            <Button
+              className="soc_icn_icn"
+              target="blank"
+              href="https://www.instagram.com/"
+            >
+              <WhatsAppIcon className="soc_icn_icn_icn" />
+            </Button>
+            <Button
+              className="soc_icn_icn"
+              target="blank"
+              href="https://www.instagram.com/"
+            >
+              <GitHubIcon className="soc_icn_icn_icn" />
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
