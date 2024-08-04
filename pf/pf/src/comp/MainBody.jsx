3c35d0a732554034fa108ab9ca6656b4ed854ed9
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-08-04 19:15:40 +0530

    home

--- pf/pf/src/comp/MainBody.jsx
+++ pf/pf/src/comp/MainBody.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Typer from "./subcomp/Typer";
 import "./MainBody.css";
+
 function MainBody() {
   return (
     <div className="main_body">
-      <div className="main_body1">herro</div>
+      <div className="main_body1">
+        <div>
+          Hi There!<span className="wave">👋 </span> <br />
+          I'M KARNEDDI LAKSHMI NARAYANA SWAROOP
+          <br />
+          <Typer
+            dataText={[
+              "MERN Stack Developer.",
+              "Software Developer.",
+              "Freelancer",
+            ]}
+          />
+        </div>
+        <div>img</div>
+      </div>
       <div className="main_body2">hdbvjdvj</div>
     </div>
   );
