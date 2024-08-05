4b1f87064de2fab3199625d347d7dca8be5ef5be
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-08-06 01:05:40 +0530

    resume

--- pf/pf/src/Resume.jsx
+++ pf/pf/src/Resume.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Resume.css";
+import Button from "@mui/material/Button";
 import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
 function Resume() {
   pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -11,11 +13,19 @@ function Resume() {
   ).toString();
   return (
     <div className="resume">
-      Resume
+      <h1>My Resume</h1>
       <br />
-      <Document file="/resume.pdf">
-        <Page pageNumber={1} />
-      </Document>
+      <div className="pdf">
+        <Document file="/resume.pdf">
+          <Page pageNumber={1} />
+        </Document>
+      </div>
+      <br />
+      <Button variant="contained" color="success">
+        <a className="reset-a" href="/resume.pdf" download="SwaroopResume.pdf">
+          <FileDownloadIcon className="reset-a_a" /> download
+        </a>
+      </Button>
     </div>
   );
 }
