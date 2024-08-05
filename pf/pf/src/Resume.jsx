afbb941a565563ffb5e3fccc97a364dbc604342d
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-08-06 00:25:10 +0530

    resume

--- pf/pf/src/Resume.jsx
+++ pf/pf/src/Resume.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./Resume.css";
+import { Document, Page } from "react-pdf";
+import { pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 function Resume() {
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    "pdfjs-dist/build/pdf.worker.min.mjs",
+    import.meta.url
+  ).toString();
   return (
     <div className="resume">
       Resume
       <br />
-      <embed src="/resume.pdf" className="pdf" />
+      <Document file="/resume.pdf">
+        <Page pageNumber={1} />
+      </Document>
     </div>
   );
 }
