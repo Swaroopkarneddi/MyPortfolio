d641f1c370a80fe7399bc4c0f9da3a34cacf2e13
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-08-12 10:30:55 +0530

    projects

--- pf/pf/src/comp/Projects.jsx
+++ pf/pf/src/comp/Projects.jsx
@@ -4,7 +4,11 @@ function Projects() {
   return (
     <div className="projects">
       <h1>My Recent Works</h1>
-      <div className="project_div"></div>
+      <div className="project_div">
+        <div className="project_card">
+          <img src="/" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
