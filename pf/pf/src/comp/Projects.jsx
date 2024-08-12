b118945b236bc322c34a77d595f0a7714d47fd01
Swaroopkarneddi <swaroopweb07@gmail.com>
2024-08-12 10:30:19 +0530

    projects

--- pf/pf/src/comp/Projects.jsx
+++ pf/pf/src/comp/Projects.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./Projects.css";
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My Recent Works</h1>
+      <div className="project_div"></div>
+    </div>
+  );
 }
 
 export default Projects;
